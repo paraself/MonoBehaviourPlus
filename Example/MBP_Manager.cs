11bbf0d21c0622d381d6e29f2b5ae95bd0550e16
Yves Wang <paraself@gmail.com>
2016-09-22 14:16:55 +0800

    re structure

---
+++ Example/MBP_Manager.cs
@@ -0,0 +1,43 @@
+﻿
+
+using UnityEngine;
+using System.Diagnostics;
+using Debug = UnityEngine.Debug;
+
+public class MBP_Manager : MonoBehaviour {
+
+	public bool isMultithreading;
+	public int calculationLoad = 200;
+	TestMBP[] instances;
+	Stopwatch sw = new Stopwatch ();
+
+
+	void Start () {
+		sw.Reset();
+		instances = Object.FindObjectsOfType<TestMBP>();
+		foreach ( var i in instances) {
+			i.maxIteration = calculationLoad;
+		}
+	}
+
+	void Update() {
+		string s = string.Empty;
+		for (int i = 0; i < instances.Length;i++) {
+			s += ("Instance : " + this.GetInstanceID() + " PI: " + instances[i].pi + "\n");
+		}
+		Debug.Log(s);
+	}
+
+	void LateUpdate() {
+		sw.Reset();
+		sw.Start();
+		if (isMultithreading)
+			TestMBP.WaitAndUpdate(10000000);
+		else
+			foreach(var i in instances) {
+				i.NotMultithread();
+			}
+		sw.Stop();
+		Debug.LogWarning("Elapsed MS:" + sw.Elapsed.Milliseconds);
+	}
+}
